aded0f060b3325afd572847c5c7b074a1b01392c
YongDaLi <yongdali314@gmail.com>
2019-06-06 17:43:02 +0800

    separating HTML and JS by external references

---
+++ JavaScript/external_references.js
@@ -0,0 +1,14 @@
+/*
+Yong Da Li
+Thursday, June 6, 2019
+
+tutorial: https://www.w3schools.com/js/js_whereto.asp
+*/
+
+function popup(){
+    alert("Surprise popup!!");
+}
+
+function change_text(){
+    document.getElementById("text").innerHTML="text changed";
+}
