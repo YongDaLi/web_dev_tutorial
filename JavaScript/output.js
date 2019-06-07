6d0d004d9c4a787204f283b999046c49897af465
YongDaLi <yongdali314@gmail.com>
2019-06-07 14:55:19 +0800

    4 ways to output

---
+++ JavaScript/output.js
@@ -0,0 +1,18 @@
+/*
+Yong Da Li
+Friday, June 7, 2019
+
+tutorial: https://www.w3schools.com/js/js_output.asp
+*/
+
+function change_text(){
+    document.getElementById("text").innerHTML="text changed";
+}
+
+function log_console(){
+    console.log("logging into console");
+}
+
+function popup(){
+    alert("This is a popup alert");
+}
